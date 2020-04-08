084bde1b60fc493779b713c803625c04728551e2
Lucas Weakland <lm.weakland@setonhill.edu>
2020-04-08 15:31:17 -0400

    Initial Commit

---
+++ Program 7 WeaklandTests/Program_7_WeaklandTests.swift
@@ -0,0 +1,34 @@
+//
+//  Program_7_WeaklandTests.swift
+//  Program 7 WeaklandTests
+//
+//  Created by Lucas Weakland on 4/8/20.
+//  Copyright © 2020 Lucas Weakland. All rights reserved.
+//
+
+import XCTest
+@testable import Program_7_Weakland
+
+class Program_7_WeaklandTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
