55f1501194c510f736e9979bf90e8e409c58306f
Lucas Weakland <lm.weakland@setonhill.edu>
2020-04-08 17:05:59 -0400

    Ready for review.

---
+++ Program 7 Weakland/tabview.swift
@@ -0,0 +1,13 @@
+//
+//  tabview.swift
+//  Program 7 Weakland
+//
+//  Created by Lucas Weakland on 4/8/20.
+//  Copyright © 2020 Lucas Weakland. All rights reserved.
+//
+
+import UIKit
+
+class tabview: UITabViewController {
+
+}
